70ade7027bb7036295f0917f5bb7c0babe91fcec
troversones <kisbencerobert2002@gmail.com>
2025-09-17 17:25:19 +0200

    2. ora faulty player class, I'll fix it later

---
+++ Testapp1/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testapp1
+{
+    enum Role
+    {
+        TOP, JGL, MID, ADC, SUPP
+    }
+    internal partial class Player: IwannaEndIt
+    {
+        public string? userName;
+
+        public int id;
+
+        public int ID
+        {
+            get; set;
+        }
+
+        public string? UserName
+        {
+            get => userName;
+            set
+            {
+                if ("asd" == value)
+                {
+                    UserName = "Kys";
+                }
+                else
+                {
+                    UserName = value;
+                }
+            }
+        }
+
+        void IwannaEndIt.ToString()
+        {
+            Console.WriteLine(this.id + " " + this.userName + " " + this.isPartial);
+        }
+    }
+    interface IwannaEndIt
+    {
+        public void ToString();
+    }
+}
--- Testapp1/Program.cs
+++ Testapp1/Program.cs
@@ -2,10 +2,60 @@
 
 namespace Testapp1
 {
+    partial class Player
+    {
+        public bool isPartial = true;
+
+        public int fun(int i)
+        {
+            return i * i;
+        }
+
+        public void fun2(ref int i, out int qubic, int def = 5)
+        {
+            i *= i;
+            qubic = i * i * i;
+        }
+    }
+
     internal class Program
     {
+        static readonly double PI;
+        const double euler = 2.77;
+
+        static Program(){ 
+            PI = Math.PI;
+        }
         static void Main(string[] args)
         {
+            //2. óra
+            Console.WriteLine(PI + " --------- " + euler);
+            int? fasz = null;
+            if (fasz.HasValue) {
+                Console.WriteLine($"value is: {fasz}");
+            }
+            else
+            {
+                Console.WriteLine("value is null");
+            }
+            //fapados megoldás az osztályra
+            var x = new { name="VIKTOR", age=22 };
+            Console.WriteLine(x.age);
+            Console.WriteLine(x.name);
+            
+            Player p = new Player();
+            p.ID = 10;
+            p.userName = "test";
+
+            int q = 25;
+            int z;
+
+            Console.WriteLine(p.ToString());
+            Console.WriteLine(p.fun(10));
+            p.fun2(ref q, out z);
+            Console.WriteLine(q);
+            Console.WriteLine(z);
+            /* 1.óra
             Console.WriteLine("Hello, World!");
             string hehe = Console.ReadLine();
             Console.WriteLine(hehe + " ezaz kristály");
@@ -17,6 +67,7 @@ namespace Testapp1
 
             log.Debug("hi");
             log.Information("hello");
-        }
+            */
+         }
     }
 }
